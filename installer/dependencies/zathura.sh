a64e9f1feabe788becb6a11f3b82ca1c50740e45
Ali Hassani <alihassanijr1998@gmail.com>
2024-03-25 22:29:47 -0400

    Allow one exception to homebrew: zathura
    Install zathura and its rediculously large dependency tree using
    homebrew instead.
    If homebrew isn't installed, just clone it and use it locally.

--- installer/dependencies/zathura.sh
+++ installer/dependencies/zathura.sh
@@ -2,8 +2,31 @@
 # Zathura
 # Document viewer
 
+install_homebrew() {
+    echo "Cloning homebrew"
+    git clone https://github.com/Homebrew/brew $HOME/.brew
+}
+
 install_zathura() {
-    echo "FAILED: Please install Zathura using homebrew."
+    echo "I can only install Zathura via homebrew."
+    echo "If you don't have homebrew, I'll install it locally and without root!"
+    echo "Do you want to proceed?"
+    read -p "Install Zathura via homebrew () [y/n]: " -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      check_and_install_dependency "brew" "$HOMEDIR/.brew/bin/brew" "install_homebrew"
+
+      brew tap zegervdv/zathura
+      brew install zathura
+      brew install zathura-pdf-poppler
+
+      mkdir -p $(brew --prefix zathura)/lib/zathura
+
+      ln -s \
+        $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib \
+        $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+    fi
 }
 
 configure_zathura() {
